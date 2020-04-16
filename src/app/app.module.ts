a1c34b195673533e5686a46668553e926979c39a
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2020-04-16 14:33:18 +0900

    Change of package jsons and updated material module loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { FormsModule } from '@angular/forms';
 // for material use in forms
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material' ;
+import { MatFormFieldModule } from '@angular/material/form-field' ;
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatInputModule } from '@angular/material/input';
 
